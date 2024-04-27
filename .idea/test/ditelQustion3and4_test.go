6c4bbb92e7817a605629d8f8801e864a1d100260
banjoSolomon <ayomidebanjo68@gmail.com>
2024-04-27 20:41:48 +0100

    first commit

--- .idea/test/ditelQustion3and4_test.go
+++ .idea/test/ditelQustion3and4_test.go
@@ -19,3 +19,28 @@ func FindLargestNumber1To10() int {
 	}
 	return largest
 }
+
+func TestTOFindTwoLargestNumbers(t *testing.T) {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expected := []int{10, 9}
+	largest1, largest2 := FindTwoLargestNumber(numbers)
+	if largest1 != expected[0] || largest2 != expected[1] {
+		t.Errorf("Expected %d, %d, got %d, %d", expected[0], expected[1], largest1, largest2)
+	}
+
+}
+
+func FindTwoLargestNumber(numbers []int) (int, int) {
+	largest1 := numbers[0]
+	largest2 := numbers[0]
+	for _, v := range numbers {
+		if v > largest1 {
+			largest2 = largest1
+			largest1 = v
+		} else if v > largest2 {
+			largest2 = v
+		}
+	}
+	return largest1, largest2
+
+}
